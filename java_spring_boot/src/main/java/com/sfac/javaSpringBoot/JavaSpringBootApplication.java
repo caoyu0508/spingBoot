ede8f5d5c6525d7078cc57c997d0ca9521873100
caoyu0508 <1178809356@qq.com>
2020-08-10 17:16:06 +0800

    "test"

--- java_spring_boot/src/main/java/com/sfac/javaSpringBoot/JavaSpringBootApplication.java
+++ java_spring_boot/src/main/java/com/sfac/javaSpringBoot/JavaSpringBootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaSpringBootApplication {
 
+    // test
     public static void main(String[] args) {
         SpringApplication.run(JavaSpringBootApplication.class, args);
     }
